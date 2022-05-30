3e36b721ba7a2aba743345dd0497ec088d5277e6
chaud1024 <chaud1024@gmail.com>
2022-05-30 23:39:02 +0900

    commit

---
+++ src/Feed.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Post from "./Post";
+
+const Feed = ({ posts }) => {
+  // console.log(posts);
+  return (
+    <>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
+
+export default Feed;
